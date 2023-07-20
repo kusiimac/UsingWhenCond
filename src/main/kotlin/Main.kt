68465b06e11fb34b5e4b503d7cbe9b2880cd8c7b
kusiimac <cleophaskusiima@gmail.com>
2023-07-20 20:01:30 +0300

    when condition

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+/**
+ * 1996-2015 -- Gen Z
+ * 1974-1995 -- Millennial
+ * 1960-1973 -- Gen X
+ * 0-1959 -- Baby Boomer
+ */
+fun main() {
+    println("Enter year if birth")
+    val year = readln().toInt()
+    when (year) {
+        in 1996..2015 -> print("Gen Z")
+        in 1974..1995 -> print("Millennial")
+        in 1960..1973 -> print("GenX")
+        else -> print("Baby Boomer")
+    }
+}
